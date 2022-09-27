2e054d569b45b8e9e2a7243e67be8ec5acc83b04
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-09-27 22:01:02 +0000

    created elements in js and added html ids and classes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+console.log('javascript')
+// Set const to body and assign id with "bodyid"
+let htmlBody = document.querySelector('body');
+htmlBody.setAttribute('id','bodyid');
+htmlBody.className = "foo";
+htmlBody.innerHTML = "Hello";
+
+// Create const mainDiv holds all html elements for weather app
+const mainDiv = document.createElement('div');
+mainDiv.setAttribute('id','main');
+
+// Create const titlediv holds all html elements for title/ 
+// User Input, The first thing the user sees
+const titleTextElem = document.createElement('div');
+titleTextElem.setAttribute('id','titleText');
+titleTextElem.className = "";
+const userForumElem = document.createElement('forum');
+userForumElem.setAttribute('id','userForum');
+userForumElem.className = "";
+const userBtnElem = document.createElement('button');
+userBtnElem.setAttribute('id','userButton');
+userBtnElem.className = "";
+
+//
 
