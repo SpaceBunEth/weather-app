db561dc6639634fca0daff3c49d1ea7b9b30a09b
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-09-28 15:44:20 +0000

    sandboxing how to write right to the DOM with js helper functions, created a func that uses a Obj to name/ create all html elements needed, looking to make a function that adds bootstrap to all elements, Maybe put it in an obj as well...

--- js/sandbox.js
+++ js/sandbox.js
@@ -9,6 +9,15 @@ const mainDiv = document.createElement('div');
 mainDiv.setAttribute('id','main');
 htmlBody.appendChild(mainDiv);
 
+// Create a set of headerDiv and bodyDiv for our content
+// Use as reference for when appending childs
+const headerDiv = document.createElement('div');
+headerDiv.setAttribute('id', 'headerDiv');
+htmlBody.appendChild(headerDiv);
+const bodyDiv = document.createElement('div');
+bodyDiv.setAttribute('id', 'bodyDiv');
+htmlBody.appendChild(bodyDiv);
+
 // Create const titlediv holds all html elements for title/ 
 // User Input, The first thing the user sees
 const titleTextElem = document.createElement('div');
@@ -31,9 +40,39 @@ userBtnElem.className = "";
 // then append element to a parent Param passed in the function
 function createElem(htmlElem, idName, parentElem){
     const makeElem = document.createElement(htmlElem);
-    madeElem.setAttribute('id', idName);
-    parentElem.appendChild(madeElem);
+    makeElem.setAttribute('id', idName);
+    parentElem.appendChild(makeElem);
 
 }
 
-createElem('div', 'anotherDiv', htmlBody);
+createElem('h1', 'anotherh1', htmlBody);
+
+// One idea is to create a ObjHtmlElement Object 
+// That holds the default states of all the html elements we need to create
+
+
+/* What are the elements we need for our start/ INIT() of our program?
+    Header div that contains all INIT() elements
+        titleText: 'h1'
+        userInputForm: 'form'
+        userInputBtn: 'button'
+Using our createElem function passing (element name, id name, parent)
+
+*/
+const listElem = {
+    titleText: 'h1',
+    userInputForm: 'form',
+    userInputBtn: 'button',
+}
+
+function ObjElemCreate(listElem) {
+    for (const key in listElem){
+        if(listElem.hasOwnProperty(key)) {
+            createElem(`${key}`,`${listElem[key]}`,headerDiv);
+        }
+    }
+}
+
+ObjElemCreate(listElem)
+
+// How do we aply class?
