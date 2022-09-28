9458eebb1ddd0e4094448bf9f3299b0769d7b0ed
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-09-28 21:27:37 +0000

    sandbox2 calling api storing information in object global variable

--- js/sandbox2.js
+++ js/sandbox2.js
@@ -1,13 +1,19 @@
 console.log('sandbox2.js')
 
-async function getUser() {
+// async function making an axios call to weather api
+// how do I get that information out and into a object?
+
+let Weather = {}
+async function getWeather() {
     try {
-      const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?zip=40069,US&appid=095fe394e44de9a81a6c44190e364b63');
-      console.log(response);
+      const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?zip=47150,US&appid=095fe394e44de9a81a6c44190e364b63');
+      Weather = response;
+      console.log(Weather)
     } catch (error) {
       console.error(error);
     }
   }
 
-
-  //095fe394e44de9a81a6c44190e364b63
+// important information
+// https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+//095fe394e44de9a81a6c44190e364b63
