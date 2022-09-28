20471e44a4c98e61aab499844a6c49e858c36d80
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-09-28 11:52:39 +0000

    appendchild in sandbox

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
+// sandbox
 console.log('javascript')
 // Set const to body and assign id with "bodyid"
 let htmlBody = document.querySelector('body');
 htmlBody.setAttribute('id','bodyid');
-htmlBody.className = "foo";
-htmlBody.innerHTML = "Hello";
 
 // Create const mainDiv holds all html elements for weather app
 const mainDiv = document.createElement('div');
 mainDiv.setAttribute('id','main');
+htmlBody.appendChild(mainDiv);
 
 // Create const titlediv holds all html elements for title/ 
 // User Input, The first thing the user sees
 const titleTextElem = document.createElement('div');
 titleTextElem.setAttribute('id','titleText');
-titleTextElem.className = "";
+titleTextElem.className = "bootstrap";
 const userForumElem = document.createElement('forum');
 userForumElem.setAttribute('id','userForum');
 userForumElem.className = "";
@@ -21,5 +21,8 @@ const userBtnElem = document.createElement('button');
 userBtnElem.setAttribute('id','userButton');
 userBtnElem.className = "";
 
-//testing append
+// testing append on body
+// htmlBody.appendChild(mainDiv)
+// htmlBody.appendChild(titleTextElem)
+
 
